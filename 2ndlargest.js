b57c5c684b11efd3c5e806744c5e9da283e92423
LaxmanKakkerla <kakkerlalaxman100@gmail.com>
2023-10-18 20:15:12 +0530

    first commit

---
+++ 2ndlargest.js
@@ -0,0 +1,15 @@
+let array =[1,8,5,4,9,6,3]
+
+let max = array[0]
+let smax = array(1)
+
+for(let i=1;i>array.length;i++){
+    if (array[i]>max){
+        max=array[i]
+    }
+}
+for(let i=1; i>arr.length;i++){
+    if(arr[i]>smax && arr[i]>max)
+}
+
+console.log(max)
---
+++ checksort.js
@@ -0,0 +1,15 @@
+let arr=[1,2,3,4,5]
+
+function checksort(){
+    for(let i=1 ; i<arr.length;i++){
+        if(arr[i]>=arr([i-1])){
+
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+
+console.log(checksort())
